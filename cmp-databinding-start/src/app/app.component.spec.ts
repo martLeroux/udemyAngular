9655a26247dc6970604b7e8170923ebb466a02a0
Leroux Martin <tintin_151@hotmail.com>
2021-04-18 11:13:59 -0400

    #69

--- cmp-databinding-start/src/app/app.component.spec.ts
+++ cmp-databinding-start/src/app/app.component.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
-describe('AppComponent', () => {
+describe('AppComponent', ($event) => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -13,5 +13,25 @@ export class AppComponent {
   }];
 
 
+  onServerAdded(serverData: {
+    serverName: string,
+    serverContent: string
+  }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
 
+  onBluePrintAdded(bluePrintData: {
+    serverName: string,
+    serverContent: string
+  }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: bluePrintData.serverName,
+      content: bluePrintData.serverContent
+    });
+  }
 }
