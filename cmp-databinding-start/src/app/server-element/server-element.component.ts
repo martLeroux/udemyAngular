5f65c6c19913e961c168cda7737c17901b5ccb97
Leroux Martin <tintin_151@hotmail.com>
2021-04-18 16:58:50 -0400

    #81

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -35,6 +35,7 @@ export class ServerElementComponent implements OnInit,
     content: string
   };
   @Input() name: string;
+  @ViewChild('heading', { static: true }) header: ElementRef;
 
   constructor() {
     console.log('constructor called');
@@ -47,7 +48,7 @@ export class ServerElementComponent implements OnInit,
 
   ngOnInit() {
     console.log('ngOnInit called!');
-    //console.log('Text Content: ' + this.header.nativeElement.textContent);
+    console.log('Text Content: ' + this.header.nativeElement.textContent);
     //console.log('Text Content of paragraph: ' + this.paragraph.nativeElement.textContent);
   }
 
@@ -66,7 +67,7 @@ export class ServerElementComponent implements OnInit,
 
   ngAfterViewInit() {
     console.log('ngAfterViewInit called!');
-    //console.log('Text Content: ' + this.header.nativeElement.textContent);
+    console.log('Text Content: ' + this.header.nativeElement.textContent);
   }
 
   ngAfterViewChecked() {
