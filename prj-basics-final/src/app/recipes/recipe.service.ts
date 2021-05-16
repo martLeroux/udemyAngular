02242a97c06c28231e7183555147d1d56e74ff32
Leroux Martin <tintin_151@hotmail.com>
2021-05-16 19:59:58 -0400

    #121

--- prj-basics-final/src/app/recipes/recipe.service.ts
+++ prj-basics-final/src/app/recipes/recipe.service.ts
@@ -1,15 +1,33 @@
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model'
 import { EventEmitter } from '@angular/core';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('Another Test Recipe', 'This is simply another test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French fries', 20)
+      ]
+    ),
+    new Recipe('Burger',
+      'This is simply another test',
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Bread', 2),
+        new Ingredient('Lettuce', 1)
+      ]
+    )
   ];
 
   getRecipes() {
+    console.log(this.recipes);
     return this.recipes.slice();
   }
 }
