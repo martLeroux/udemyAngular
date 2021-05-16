058641ba1b39cdf0217f95891e53d07ce5ea56b2
Leroux Martin <tintin_151@hotmail.com>
2021-05-16 18:55:59 -0400

    #114

--- services-start/src/app/new-account/new-account.component.ts
+++ services-start/src/app/new-account/new-account.component.ts
@@ -10,7 +10,9 @@ import { AccountsService } from '../accounts.service';
 })
 export class NewAccountComponent {
   constructor(private loggingService: LoggingService,
-    private accountsService: AccountsService) { }
+    private accountsService: AccountsService) {
+    this.accountsService.statusUpdated.subscribe((status: string) => alert('New Status:' + status))
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountsService.addAccount(accountName, accountStatus);
