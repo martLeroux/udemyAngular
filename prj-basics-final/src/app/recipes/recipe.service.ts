258bdf10a75d63ca462b6401bfb1faf5bc085f43
Leroux Martin <tintin_151@hotmail.com>
2021-05-16 20:10:53 -0400

    #122

--- prj-basics-final/src/app/recipes/recipe.service.ts
+++ prj-basics-final/src/app/recipes/recipe.service.ts
@@ -1,7 +1,9 @@
 import { Recipe } from './recipe.model';
 import { Ingredient } from '../shared/ingredient.model'
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
@@ -26,8 +28,14 @@ export class RecipeService {
     )
   ];
 
+  constructor(private slService: ShoppingListService) { }
+
   getRecipes() {
     console.log(this.recipes);
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
