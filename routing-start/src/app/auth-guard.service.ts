229656622d39d57e519ec8b8d52349f6b296c64c
Leroux Martin <tintin_151@hotmail.com>
2021-05-30 19:54:42 -0400

    #148

--- routing-start/src/app/auth-guard.service.ts
+++ routing-start/src/app/auth-guard.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from "@angular/core";
 import { AuthService } from "./auth.service";
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
   constructor(private authService: AuthService, private router: Router) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
@@ -17,5 +17,9 @@ export class AuthGuard implements CanActivate {
         }
       }
     );
+  }
+
+  canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    return this.canActivate(route, state);
   }
 }
