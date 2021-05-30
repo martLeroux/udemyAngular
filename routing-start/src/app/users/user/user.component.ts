2c6674905a84f162a4e0be635903a51bd4e2aefd
Leroux Martin <tintin_151@hotmail.com>
2021-05-30 19:05:11 -0400

    #140

--- routing-start/src/app/users/user/user.component.ts
+++ routing-start/src/app/users/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Subscription } from 'rxjs/Subscription';
 
