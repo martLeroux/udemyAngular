c361a304d2f0643241403d316125e5911dd1999b
Leroux Martin <tintin_151@hotmail.com>
2021-05-30 19:33:24 -0400

    #144

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from "@angular/router";
-
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -13,24 +11,9 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { PagepageNotFoundComponent } from './pagepage-not-found/pagepage-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
 
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  {
-    path: 'users', component: UsersComponent, children: [
-      { path: ':id/:name', component: UserComponent },
-    ]
-  },
 
-  {
-    path: 'servers', component: ServersComponent, children: [
-      { path: ':id', component: ServerComponent },
-      { path: ':id/edit', component: EditServerComponent },
-    ]
-  },
-  { path: 'not-found', component: PagepageNotFoundComponent },
-  { path: '**', redirectTo: 'not-found' }
-];
 
 @NgModule({
   declarations: [
@@ -46,7 +29,7 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
