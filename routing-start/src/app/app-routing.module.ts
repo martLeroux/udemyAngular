9398f246547594268147d1256969d3108266e617
Leroux Martin <tintin_151@hotmail.com>
2021-06-06 20:42:41 -0400

    #152

--- routing-start/src/app/app-routing.module.ts
+++ routing-start/src/app/app-routing.module.ts
@@ -38,6 +38,7 @@ const appRoutes: Routes = [
 
 @NgModule({
   imports: [
+    //RouterModule.forRoot(appRoutes, { useHash: true })
     RouterModule.forRoot(appRoutes)
   ], exports: [RouterModule]
 })
