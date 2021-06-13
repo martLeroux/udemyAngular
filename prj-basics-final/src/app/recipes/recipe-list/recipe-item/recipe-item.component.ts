f230938036eff8899ee63e22743dd233eec5f061
Leroux Martin <tintin_151@hotmail.com>
2021-06-13 19:27:47 -0400

    #161

--- prj-basics-final/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ prj-basics-final/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../../recipe.model';
-import { RecipeService } from '../../recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -10,13 +9,8 @@ import { RecipeService } from '../../recipe.service';
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
 
-  constructor(private recipeSerive: RecipeService) { }
-
   ngOnInit() {
   }
 
-  onSelected() {
-    this.recipeSerive.recipeSelected.emit(this.recipe);
-  }
 
 }
