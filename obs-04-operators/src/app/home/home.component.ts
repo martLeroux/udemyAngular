fbdac1a196af157a9dc01e20d801483c712041a6
Leroux Martin <tintin_151@hotmail.com>
2021-06-20 16:46:30 -0400

    #175

--- obs-04-operators/src/app/home/home.component.ts
+++ obs-04-operators/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { interval, Subscription, Observable } from 'rxjs';
 import { map, filter } from 'rxjs/operators';
@@ -12,17 +12,18 @@ export class HomeComponent implements OnInit, OnDestroy {
 
   private firstObsSubscription: Subscription;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
-    /*this.firstObsSubscription = interval(1000).subscribe(count => {
-      console.log(count);
-    });*/
+    // this.firstObsSubscription = interval(1000).subscribe(count => {
+    //   console.log(count);
+    // });
     const customIntervalObservable = Observable.create(observer => {
       let count = 0;
       setInterval(() => {
         observer.next(count);
-        if (count === 2) {
+        if (count === 5) {
           observer.complete();
         }
         if (count > 3) {
@@ -33,24 +34,21 @@ export class HomeComponent implements OnInit, OnDestroy {
     });
 
 
-
     this.firstObsSubscription = customIntervalObservable.pipe(filter(data => {
       return data > 0;
-    }), map(data => {
-      return 'Round: ' + (+data + 1);
-    })).subscribe(count => {
-      console.log(count);
+    }), map((data: number) => {
+      return 'Round: ' + (data + 1);
+    })).subscribe(data => {
+      console.log(data);
     }, error => {
-      console.log(error.message);
+      console.log(error);
+      alert(error.message);
     }, () => {
       console.log('Completed!');
     });
   }
 
-
-
-  //No need to unsubscribe if complete
-  ngOnDestroy() {
+  ngOnDestroy(): void {
     this.firstObsSubscription.unsubscribe();
   }
 
