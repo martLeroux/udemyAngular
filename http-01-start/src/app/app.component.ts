3dd8a01301348565dc9d906395bf67d621837109
Leroux Martin <tintin_151@hotmail.com>
2021-10-11 20:46:10 -0400

    #259

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -10,6 +10,7 @@ import { Post } from './post.model'
 })
 export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
+  isFetching = false;
 
   constructor(private http: HttpClient) { }
 
@@ -33,6 +34,7 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
+    this.isFetching = true;
     this.http
       .get<{ [key: string]: Post }>('https://ng-complete-guide-ae381-default-rtdb.firebaseio.com/posts.json')
       .pipe(
@@ -47,6 +49,7 @@ export class AppComponent implements OnInit {
           return postsArray;
         }))
       .subscribe(posts => {
+        this.isFetching = false;
         console.log(posts[0]);
         this.loadedPosts = posts;
       });
