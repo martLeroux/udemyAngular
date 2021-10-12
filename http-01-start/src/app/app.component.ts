dabc4a8c578d79b0408b4bb667ea9664f599eed0
Leroux Martin <tintin_151@hotmail.com>
2021-10-11 20:28:53 -0400

    #256

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -31,7 +32,18 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
-    this.http.get('https://ng-complete-guide-ae381-default-rtdb.firebaseio.com/posts.json').subscribe(posts => {
+    this.http.get('https://ng-complete-guide-ae381-default-rtdb.firebaseio.com/posts.json').pipe(
+      map(responseData => {
+        const postsArray = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            postsArray.push({ ...responseData[key], });
+          }
+
+        }
+        return postsArray;
+      })
+    ).subscribe(posts => {
       console.log(posts);
     });
   }
