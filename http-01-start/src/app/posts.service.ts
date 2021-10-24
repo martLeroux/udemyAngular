9c3dfbddeec4e4ec6d0b807c040a15d817bff855
Leroux Martin <tintin_151@hotmail.com>
2021-10-24 19:34:47 -0400

    #267

--- http-01-start/src/app/posts.service.ts
+++ http-01-start/src/app/posts.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Post } from "./post.model"
-import { map } from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
+import { Subject, throwError } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class PostsService {
+
+  error = new Subject<string>();
 
   constructor(private http: HttpClient) { }
 
@@ -16,6 +19,8 @@ export class PostsService {
         postData
       ).subscribe(responseData => {
         console.log(responseData);
+      }, error => {
+        this.error.next(error.message);
       });
   }
 
@@ -32,6 +37,9 @@ export class PostsService {
 
           }
           return postsArray;
+        }), catchError(errorRes => {
+          // Send to analytics server
+          return throwError(errorRes);
         }))
       ;
   }
