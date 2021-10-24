d38265ccece93cd47ed159a3c4b1e7d738cca77b
Leroux Martin <tintin_151@hotmail.com>
2021-10-24 19:52:25 -0400

    #270

--- http-01-start/src/app/posts.service.ts
+++ http-01-start/src/app/posts.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams, HttpEventType } from '@angular/common/http';
 import { Post } from "./post.model"
-import { map, catchError } from 'rxjs/operators';
+import { map, catchError, tap } from 'rxjs/operators';
 import { Subject, throwError } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
@@ -16,7 +16,10 @@ export class PostsService {
     this.http
       .post<{ name: string }>(
         'https://ng-complete-guide-ae381-default-rtdb.firebaseio.com/posts.json',
-        postData
+        postData,
+        {
+          observe: 'response'
+        }
       ).subscribe(responseData => {
         console.log(responseData);
       }, error => {
@@ -52,6 +55,17 @@ export class PostsService {
   }
 
   deletePosts() {
-    return this.http.delete('https://ng-complete-guide-ae381-default-rtdb.firebaseio.com/posts.json')
+    return this.http.delete('https://ng-complete-guide-ae381-default-rtdb.firebaseio.com/posts.json',
+      {
+        observe: 'events'
+      }).pipe(tap(event => {
+        console.log(event);
+        if (event.type === HttpEventType.Sent) {
+          //...
+        }
+        if (event.type === HttpEventType.Response) {
+          console.log(event.body);
+        }
+      }))
   }
 }
