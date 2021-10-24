9c33715f0619892a4e860141a39c91a4b241e1b6
Leroux Martin <tintin_151@hotmail.com>
2021-10-24 19:17:12 -0400

    #264

--- http-01-start/src/app/posts.service.ts
+++ http-01-start/src/app/posts.service.ts
@@ -35,4 +35,8 @@ export class PostsService {
         }))
       ;
   }
+
+  deletePosts() {
+    return this.http.delete('https://ng-complete-guide-ae381-default-rtdb.firebaseio.com/posts.json')
+  }
 }
