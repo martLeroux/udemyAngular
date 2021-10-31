806f1c041c9ddb7c6031c5c6b6318f054aa55c73
Leroux Martin <tintin_151@hotmail.com>
2021-10-31 19:18:39 -0400

    #284/285

--- prj-basics-final/src/app/shared/data-storage.service.ts
+++ prj-basics-final/src/app/shared/data-storage.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from '@angular/common/http';
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
@@ -18,14 +18,16 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http.get<Recipe[]>('https://ng-course-recipe-book-27ea0-default-rtdb.firebaseio.com/recipes.json')
+    return this.http.get<Recipe[]>('https://ng-course-recipe-book-27ea0-default-rtdb.firebaseio.com/recipes.json')
       .pipe(map(recipes => {
         return recipes.map(recipe => {
           return { ...recipe, ingredients: recipe.ingredients ? recipe.ingredients : [] }
         })
-      }))
-      .subscribe(recipes => {
-        this.recipesServices.setRecipes(recipes);
-      })
+      }),
+        tap(recipes => {
+          this.recipesServices.setRecipes(recipes);
+        })
+
+      );
   }
 }
