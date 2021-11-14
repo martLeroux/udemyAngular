46e59d3df4b774bff10cdd105edf02604680c9ae
Leroux Martin <tintin_151@hotmail.com>
2021-11-14 18:10:51 -0500

    #298

--- prj-basics-final/src/app/auth/auth.service.ts
+++ prj-basics-final/src/app/auth/auth.service.ts
@@ -3,12 +3,13 @@ import { HttpClient } from "@angular/common/http";
 import { catchError } from "rxjs/operators";
 import { throwError } from 'rxjs';
 
-interface AuthResponseData {
-  idToken: string,
-  email: string,
-  refreshToken: string,
-  expiresIn: string,
-  localId: string
+export interface AuthResponseData {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: boolean;
 }
 
 @Injectable({ providedIn: 'root' })
@@ -33,5 +34,14 @@ export class AuthService {
         }
         return throwError(errorMessage);
       }))
+  }
+
+  login(email: string, password: string) {
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDmXTAXHPOAHGJYfucVd0UwHzcRukIZ2H8',
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      })
   }
 }
