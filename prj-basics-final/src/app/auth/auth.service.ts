2344c792f9aa52939984c4e50f4c35e09c5bd241
Leroux Martin <tintin_151@hotmail.com>
2021-11-14 18:21:48 -0500

    #299

--- prj-basics-final/src/app/auth/auth.service.ts
+++ prj-basics-final/src/app/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { catchError } from "rxjs/operators";
 import { throwError } from 'rxjs';
 
@@ -22,18 +22,7 @@ export class AuthService {
         email: email,
         password: password,
         returnSecureToken: true
-      }).pipe(catchError(errorRes => {
-        let errorMessage = 'An unknown error occured';
-        console.log(errorRes);
-        if (!errorRes.error || !errorRes.error.error) {
-          return throwError(errorMessage);
-        }
-        switch (errorRes.error.error.message) {
-          case 'EMAIL_EXISTS':
-            errorMessage = 'This email exists already';
-        }
-        return throwError(errorMessage);
-      }))
+      }).pipe(catchError(this.handleError))
   }
 
   login(email: string, password: string) {
@@ -42,6 +31,26 @@ export class AuthService {
         email: email,
         password: password,
         returnSecureToken: true
-      })
+      }).pipe(catchError(this.handleError))
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = 'An unknown error occured';
+    console.log(errorRes);
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorRes.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email exists already';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'This email/password combination does not exist.';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'This email/password combination does not exist.';
+        break;
+    }
+    return throwError(errorMessage);
   }
 }
