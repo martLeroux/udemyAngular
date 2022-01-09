c5d9c29d818ec16c9a6d87bc1319a16f76d4cc7b
Martin Leroux <tintin_151@hotmail.com>
2022-01-09 16:28:17 -0500

    #328

--- prj-basics-final/src/app/shopping-list/shopping-list.module.ts
+++ prj-basics-final/src/app/shopping-list/shopping-list.module.ts
@@ -1,7 +1,7 @@
-import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
+import { SharedModule } from "../shared/shared.module";
 import { ShoppingEditComponent } from "./shopping-edit/shopping-edit.component";
 import { ShoppingListRoutingModule } from "./shopping-list-routing.module";
 import { ShoppingListComponent } from "./shopping-list.component";
@@ -12,7 +12,7 @@ import { ShoppingListComponent } from "./shopping-list.component";
         ShoppingListComponent,
         ShoppingEditComponent,
     ],
-    imports: [RouterModule, CommonModule, FormsModule, ShoppingListRoutingModule],
+    imports: [RouterModule, SharedModule, FormsModule, ShoppingListRoutingModule],
 })
 export class ShoppingListModule {
 
