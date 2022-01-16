16d5cad02036794bcaea9054b0615826bed0ce39
Martin Leroux <tintin_151@hotmail.com>
2022-01-16 16:31:55 -0500

    #336

--- prj-basics-final/src/app/shopping-list/shopping-list.component.ts
+++ prj-basics-final/src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
+import { LoggingService } from '../logging.service';
 
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
@@ -17,7 +18,7 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
 
   private subscription: Subscription;
 
-  constructor(private slService: ShoppingListService) { }
+  constructor(private slService: ShoppingListService, private logginService: LoggingService) { }
 
   ngOnInit() {
     this.ingredients = this.slService.getIngredients();
@@ -27,6 +28,8 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
           this.ingredients = ingredients;
         }
       )
+
+    this.logginService.printLog('Hello from ShoppingList compononent ngonInit')
   }
 
   onEditItem(index: number) {
