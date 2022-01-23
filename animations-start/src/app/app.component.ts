8f55dc0cb086418ac116d821b1d0431985fcfde6
Martin Leroux <tintin_151@hotmail.com>
2022-01-23 18:07:37 -0500

    #408

---
+++ animations-start/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { state, style, trigger } from '@angular/animations';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('divState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0)'
+      })),
+      state('highlighted', style({
+        backgroundColor: 'blue',
+        transform: 'translateX(100px)'
+      }))
+    ])
+  ]
+})
+export class AppComponent {
+  state = 'normal';
+  list = ['Milk', 'Sugar', 'Bread'];
+
+  onAnimate() {
+    this.state == 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+    //this.wildState == 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
+  }
+
+  onAdd(item) {
+    this.list.push(item);
+  }
+}
